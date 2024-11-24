791f8061ef7c12d154d7c958460eea121e9c5263
Preston <prestonherr@gmail.com>
2024-11-24 09:46:31 -0600

    Edit profile function

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,19 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editCloseButton = document.querySelector(".modal__close-button");
+
+function openModal() {
+  editProfileModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editProfileModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+editCloseButton.addEventListener("click", closeModal);
